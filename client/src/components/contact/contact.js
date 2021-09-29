0462ddc679a38ab4580a0b6fed174501170a149b
Adrian <adrian.auch11@gmail.com>
2021-09-29 17:27:29 -0600

    finsihed very rough outline of portfolio

--- client/src/components/contact/contact.js
+++ client/src/components/contact/contact.js
@@ -1,5 +1,53 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 export const contact = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Col>
+            <Card className="text-center">
+              <Card.Header>Featured</Card.Header>
+              <Card.Body>
+                <Card.Title>Special title treatment</Card.Title>
+                <Card.Text>
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+              <Card.Footer className="text-muted">2 days ago</Card.Footer>
+            </Card>
+          </Col>
+          <Col>
+            <Form>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlInput1"
+              >
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="name@example.com" />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
+
+export default contact;
