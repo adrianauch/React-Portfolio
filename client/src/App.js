eb489014bde46a6d9a05e01fc439b3875ff0290d
Adrian <adrian.auch11@gmail.com>
2021-10-05 23:38:38 -0600

    fixed routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,53 +4,40 @@ import NavBar from "./components/Navbar/Navbar";
 import AboutMe from "./components/about/about";
 import Work from "./components/work/work";
 import ContactMe from "./components/contact/contact";
-import Hero from "./components/hero";
+// import Hero from "./components/hero";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
-  useQuery,
-  gql,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
-const client = new ApolloClient({
-  uri: "/graphql",
-  cache: new InMemoryCache(),
-});
+import { Route, Switch, Link } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   createHttpLink,
+//   useQuery,
+//   gql,
+// } from "@apollo/client";
+// import { setContext } from "@apollo/client/link/context";
+// const client = new ApolloClient({
+//   uri: "/graphql",
+//   cache: new InMemoryCache(),
+// });
 
 // In our main App component, we are rendering only single instances of Header and Navbar and several instances of Card
 function App() {
   return (
-    <ApolloProvider client={client}>
-      <Router>
-        <NavBar />
-        <Route exact path="/aboutme">
-          <AboutMe />
-        </Route>
+    <div>
+      <NavBar />
 
-        <Route exact path="/">
-          <AboutMe />
-        </Route>
-        <Route exact path="/work">
-          <Work />
-        </Route>
-        <Route exact path="/contact-me">
-          <ContactMe />
-        </Route>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
-        <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
 
-        <Footer />
-      </Router>
-    </ApolloProvider>
+      <Footer />
+    </div>
   );
 }
 
